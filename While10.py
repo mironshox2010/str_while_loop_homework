f00eff3aa4d15b8714443ac0b8fb9f0e60b580e7
mironshox2010 <mironshoxcodeschool@gmail.com>
2023-04-17 13:32:28 +0500

    While10.py

--- While10.py
+++ While10.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    d = 0
+    count = 0
+    while d < len(s):
+        if s[d].isdigit() and int(s[d])%2==1:
+            count += int(s[d]) 
+        d += 1 
+    return count
+print(main('497936735'))
